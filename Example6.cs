9ffc8746e9a7c9c014d6484941e3a90517b81b8f
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 15:12:07 +0200

    Example6

--- Example6.cs
+++ Example6.cs
@@ -5,3 +5,11 @@ Expected input and output
 "learn", "current", "deal" → "l*rn", "current", "d*l"
 
 */
+
+/* Solution */
+  List<string> names = new List<string>() { "learn", "current", "deal" };
+  var res = names.Select(x=>x.Contains("ea")?x.Replace("ea","*"):x);
+  foreach (var n in res)
+  {
+      Console.Write(n + " ");   //l*rn current d*l
+  }
