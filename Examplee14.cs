079a37ce2cf43746cdf795e123e551dffdb92b5f
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 17:01:33 +0200

    Example14

--- Examplee14.cs
+++ Examplee14.cs
@@ -7,3 +7,18 @@ Expected input and output
 "gamma" → "Letter g occurs 1 time(s), Letter a occurs 2 time(s), Letter m occurs 2 time(s)"
 
 */
+
+/*Solution*/
+var str = "MonaMMOOAAAAAAA";
+var res = str.ToLower().GroupBy(x => x);
+foreach (var item in res)
+{
+    Console.WriteLine($"{item.Key}  {item.Count()}");
+}
+
+/*Output
+m  3
+o  3
+n  1
+a  8
+*/
