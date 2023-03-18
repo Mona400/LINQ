310edc060f0624c70b19d6d14445d3ece32833d9
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 16:51:56 +0200

    Example13

--- Exampled13.cs
+++ Exampled13.cs
@@ -6,5 +6,13 @@ Expected input and output
 [1, 2, 3], [4, 5, 6] → 32 [7, -9, 3, -5], [9, 1, 0, -4] → 74
 
 
-
 */
+
+/* Solution */
+
+  List<int> list1 = new List<int>() { 1,2,3};
+
+  List<int> list2 = new List<int>() { 4,5,6};
+
+  var res = list1.Zip(list2,(a,b)=>a*b).Sum();
+  Console.WriteLine(  res);
