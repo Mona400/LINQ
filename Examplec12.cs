e773a109a6f5c8bc4fab110f8faec1435601ede8
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 16:39:54 +0200

    Example12

--- Examplec12.cs
+++ Examplec12.cs
@@ -6,3 +6,11 @@ Expected input and output
 "DDD example CQRS Event Sourcing" → DDD, CQRS
 
 */
+
+/*Solution*/
+var str = "HI My this THwww hello HE MONA ma";
+var uppercaseOnly = str.Split(' ').Where(x=>string.Equals(x, x.ToUpper()));
+foreach (var item in uppercaseOnly)
+{
+    Console.WriteLine(  item);
+}
