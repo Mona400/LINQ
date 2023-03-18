4136cae377997df377b01ff36e1aedcd67fea916
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 14:45:11 +0200

    Update Examplef16.cs

--- Examplef16.cs
+++ Examplef16.cs
@@ -1,1 +1,9 @@
 
+/*
+Example 16:
+
+Write a query that returns double letters sequence in format: AA AB AC ... ZX ZY ZZ
+Expected input and output
+(no input) → "AA AB AC ... AZ BA BB ... ZX ZY ZZ"
+
+*/
