eab1fb9834b0af8b7c17463ae0ef8b6a3d058abc
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 14:31:57 +0200

    Create Example10.cs

---
+++ Example10.cs
@@ -0,0 +1,9 @@
+/*
+Example 10:
+
+Write a query that returns most frequent character in string. Assume that there is only one such character.
+Expected input and output
+"panda" → 'a' "n093nfv034nie9"→ 'n'
+
+
+*/
