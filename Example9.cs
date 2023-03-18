a517e21751c894efb5b20d0e8507c74d763eb0d3
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 16:11:54 +0200

    Example9

--- Example9.cs
+++ Example9.cs
@@ -1,8 +1,18 @@
 /*
 Example 9:
-Given a non-empty string consisting only of special chars (!, @, # etc.), return a number (as a string) where each digit corresponds to given special char on the keyboard ( 1→ !, 2 → @, 3 → # etc.).
+Given a non-empty string consisting only of special chars (!, @, # etc.), return a number (as a string)
+where each digit corresponds to given special char on the keyboard ( 1→ !, 2 → @, 3 → # etc.).
 Expected input and output
 "())(" → "9009" "*$(#&" → "84937" "!!!!!!!!!!" → "1111111111"
 
 
 */
+/*Solution*/
+
+ var ch=new char[] { ')', '!', '@', '#', '$', '%', '^', '&', '*', '('};
+
+  var encryptedNumber = "#(@*%)$(&$*#&";
+
+  var decryptedNumber =string.Join("", encryptedNumber.Select(x => Array.IndexOf(ch, x)));
+
+  Console.WriteLine(decryptedNumber); // 3928504974837
