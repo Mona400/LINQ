863672098c1db2d07dbfa4755bbabdc7f5e1ea0c
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 14:31:08 +0200

    Create Example9.cs

---
+++ Example9.cs
@@ -0,0 +1,8 @@
+/*
+Example 9:
+Given a non-empty string consisting only of special chars (!, @, # etc.), return a number (as a string) where each digit corresponds to given special char on the keyboard ( 1→ !, 2 → @, 3 → # etc.).
+Expected input and output
+"())(" → "9009" "*$(#&" → "84937" "!!!!!!!!!!" → "1111111111"
+
+
+*/
