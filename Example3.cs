5e7e1d522e85fef409e1e5e3856ecb3ebfe45c3c
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 14:12:18 +0200

    Create Example3.cs
    Example3

---
+++ Example3.cs
@@ -0,0 +1,15 @@
+/*
+Write a query that returns words starting with letter 'a' and ending with letter 'm'.
+Expected input and output
+"mum", "amsterdam", "bloom" → "amsterdam"
+
+*/
+
+/*Solution*/
+
+List<string> names = new List<string>() { "amonam","abdelmonem","alim","mostafa","abdlahamn","Amamm"};
+var res = names.Select(x=>x.ToLower()).Where(x=>x.StartsWith('a')).Where(x=>x.EndsWith('m'));
+foreach (var n in res)
+{
+    Console.Write(n+" ");   //amonam abdelmonem alim amamm
+}
