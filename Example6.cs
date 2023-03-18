1ccaea846331ced9d5dd4ac076bae48a45692b61
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 14:28:27 +0200

    Create Example6.cs

---
+++ Example6.cs
@@ -0,0 +1,7 @@
+/*
+Example 6:
+Write a query that replaces 'ea' substring with astersik (*) in given list of words.
+Expected input and output
+"learn", "current", "deal" → "l*rn", "current", "d*l"
+
+*/
