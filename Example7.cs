909a4d99c5a4130ffce7198b539a779588cf8c23
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 14:29:16 +0200

    Create Example7.cs

---
+++ Example7.cs
@@ -0,0 +1,7 @@
+/*
+Example 7:
+Given a non-empty list of words, sort it alphabetically and return a word that contains letter 'e'.
+Expected input and output
+["plane", "ferry", "car", "bike"]→ "plane"
+
+*/
