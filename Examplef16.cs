6062ef855af10447a1e182456c937748931da143
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 17:56:10 +0200

    Example16

--- Examplef16.cs
+++ Examplef16.cs
@@ -7,3 +7,12 @@ Expected input and output
 (no input) → "AA AB AC ... AZ BA BB ... ZX ZY ZZ"
 
 */
+
+ var doubleLetters = Enumerable.Range((char)65, 26)
+                       .SelectMany(x => Enumerable.Range((char)65, 26).Select(y => (char)x + "" + (char)y));
+
+            foreach (var doubleLetter in doubleLetters)
+            {
+                Console.Write(doubleLetter + " "); // AA AB AC ... AZ BA BB ... ZX ZY ZZ
+            }
+
