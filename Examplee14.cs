d1f2b5535f8251ac26defbfa16620c3838935699
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 14:43:29 +0200

    Update Examplee14.cs

--- Examplee14.cs
+++ Examplee14.cs
@@ -1,1 +1,9 @@
 
+/*
+Example 14:
+
+Write a query that returns letters and their frequencies in the string.
+Expected input and output
+"gamma" → "Letter g occurs 1 time(s), Letter a occurs 2 time(s), Letter m occurs 2 time(s)"
+
+*/
