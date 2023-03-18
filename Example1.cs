5f4df02f44bf8dddecdaf77e14ecc0055d57500f
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 08:48:10 +0200

    Create Example1.cs

---
+++ Example1.cs
@@ -0,0 +1,10 @@
+
+/*
+Example 1:
+
+Given an array of integers, write a query that returns list of numbers greater than 30 and less than 100.
+Expected input and output
+[67, 92, 153, 15] → 67, 92
+
+*/
+
