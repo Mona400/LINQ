223db164f85dcd472e818fb2ec198a6ea5089059
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 14:59:50 +0200

    Example5

--- Example5.cs
+++ Example5.cs
@@ -6,3 +6,12 @@ Expected input and output
 [7, 2, 30] → 7 - 49, 30 – 900
 
 */
+
+/*Solution*/
+
+List<int>numbers=new List<int>() { 7, 2, 30 };
+var res = numbers.Where(x => x * x > 20);
+foreach(int x in res)
+{
+    Console.Write($"{x} ");  //7 30
+}
