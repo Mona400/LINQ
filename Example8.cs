7b0d4ca62ed4ed08e9ad529813d3b6268c53d6d0
Mona Abdelmonem <89868260+Mona400@users.noreply.github.com>
2023-03-18 14:30:08 +0200

    Create Example8.cs

---
+++ Example8.cs
@@ -0,0 +1,9 @@
+/*
+Example 8:
+
+Write a query that shuffles sorted array.
+Expected input and output
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] → [4, 9, 3, 5, 2, 10, 1, 6, 8, 7] [38, 24, 8, 0, -1, -17, -33, -100] → [0, -100, -17, 38, 8, -1, 24, -33,]
+
+
+*/
